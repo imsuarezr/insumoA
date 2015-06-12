acbd9beae2dcff24c19a749d15d00b469e1c7717
ElectroGio <giovannippk7@hotmail.com>
2015-06-12 16:38:45 -0500

    Revert "Inicio de proyecto"
    This reverts commit ef00720f6ee51a3b5cd38a5b859b2453144f5fec.

--- fuentes_agua/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(NacimientosAgua)
-admin.site.register(ViviendaUsos)
--- lacteos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Comprador)
-admin.site.register(ProduccionLeche)
-admin.site.register(Suplementos)
-admin.site.register(Insumos)
